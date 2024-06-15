a5c8d30d74b6509f67ce8ca5240edc1413f08023
sherazanwar21 <sheraz786868@gmail.com>
2024-06-15 21:31:17 +0000

    s3

--- eks-modules/providers.tf
+++ eks-modules/providers.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region                   = "us-east-2"
   shared_credentials_files = ["~/.aws/credentials"]
 }
+
+terraform {
+  backend "s3" {
+    bucket = "gha-tf-sheraz"
+    key = "statefiles2/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
